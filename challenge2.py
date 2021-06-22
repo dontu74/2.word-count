aeea99a3feb2f10554108049796cc2a220a9f043
Don Tu <55115387+dontu74@users.noreply.github.com>
2021-06-22 02:41:35 -0400

    first draft

---
+++ challenge2.py
@@ -0,0 +1,48 @@
+# Import the python sys library to read a file
+
+import sys
+
+# assign an empty dictionary as word_string
+# using the with open() in the sys library to read an external file name " demo.txt" as a f variable
+# use the read () and split () to read the demo.txt and separate each word as string and assigned 
+# into the empty dictionary word_string
+word_string =  {}
+with open('demo.txt') as f:
+    word_string = f.read().split()
+print(word_string)  # use print to debug and see if it print out each word as a string
+
+# Create a function call to count how many characters in the file
+def count_char ():
+    
+
+    
+    return
+
+# Create a function call to cound how many words in the file
+
+def count_words ():
+
+
+    return 
+
+# Create a function call to cound how many sentence in the file
+def count_sent ():
+
+
+
+    return
+
+# Create a function call to cound how many paragraph in the file
+
+def count_para ():
+
+
+    return
+
+# Create a function call to cound how many unique word in the file
+
+def count_bigr ():
+
+
+
+    return
