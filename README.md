cd868289bef3e85ae46eec2b6e8905fb8b98e7f7
Don Tu <55115387+dontu74@users.noreply.github.com>
2021-06-23 23:52:41 -0400

    adding function calls

--- README.md
+++ README.md
@@ -4,68 +4,61 @@ Challenge #2 - Word Count challenge
 
 ## Description
 
-Prompt: Did you know you can access a word count feature on Google Docs by pressing Control + Shift
-+ C? We want you to re-implement word count, with a twist. Given a piece of text, return the following
-statistics:
-1. Character Count
-2. Word Count
-3. Sentence Count
-4. Paragraph Count
-5. Bigrams (counts of unique pairs of words)
-6. Come up with your own features! Use this as an opportunity to show off! We’ll be impressed by
-creative, well-presented solutions.
+Prompt: Did you know you can access a word count feature on Google Docs by pressing Control + Shift + C 
+We want you to re-implement word count, with a twist. Given a piece of text, return the following statistics:
+### 1. Character Count
+* Create a function call to count how many characters in the file
+![Solution](images/character.jpg)
+### 2. Word Count
+* Create a function call to count how many words in the file
+![Solution](images/word.jpg)
+### 3. Sentence Count
+* Create a function call to count how many sentence in the file
+![Solution](images/sentence.jpg)
+### 4. Paragraph Count
+* Create a function call to count how many paragraph in the file
 
+![Solution](images/paragraph.jpg)
+### 5. Bigrams (counts of unique pairs of words)
+* Create a function call to count how many unique word in the file
+
+![Solution](images/bigr.jpg)
+### 6. Come up with your own features! Use this as an opportunity to show off! We’ll be impressed by creative, well-presented solutions.
+* This function will replace all the "a" or "A" to "@" and all the "e" and "E" to "3"
+
+![Solution](images/funker2.jpg)
+![Solution](images/funker.jpg)
 ## Getting Started
 
-### Dependencies
-
-* Describe any prerequisites, libraries, OS version, etc., needed before installing program.
-* ex. Windows 10
+* Must have Python program install
 
 ### Installing
 
-* How/where to download your program
-* Any modifications needed to be made to files/folders
+* Go to - https://github.com/dontu74/2.word-count
+
+* Download 2 files from the above github link: challenge2.py and demo.txt
 
 ### Executing program
 
-* How to run the program
-* Step-by-step bullets
+* Make sure the demo.txt is on the same directory as the challenge2.py
 ```
-code blocks for commands
-```
-
-## Help
-
-Any advise for common problems or issues.
-```
-command to run if program contains helper info
+py .\challege2.py
 ```
 
 ## Authors
 
 Contributors names and contact info
 
-ex. Dominique Pizzie  
-ex. [@DomPizzie](https://twitter.com/dompizzie)
+Don Tu
+[@dontu - Github](https://github.com/dontu74)
+[@dontu - replit](https://replit.com/@dontu74)
 
-## Version History
-
-* 0.2
-    * Various bug fixes and optimizations
-    * See [commit change]() or See [release history]()
-* 0.1
-    * Initial Release
-
-## License
-
-This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
 
 ## Acknowledgments
 
-Inspiration, code snippets, etc.
-* [awesome-readme](https://github.com/matiassingers/awesome-readme)
-* [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
-* [dbader](https://github.com/dbader/readme-template)
-* [zenorocha](https://gist.github.com/zenorocha/4526327)
-* [fvcproductions](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46)
+### Learning Resources:
+* [w3schools](https://www.w3schools.com/python/python_regex.asp#matchobject)
+* [w3schools - Read Files](https://www.w3schools.com/python/python_file_open.asp)
+* [Stackoverflow - Python users' forum](https://stackoverflow.com/questions/7359510/how-to-count-the-number-of-words-in-a-paragraph-and-exclude-some-words-from-a-f)
+* [Geeksforgeeks - Python users' forum](https://www.geeksforgeeks.org/counting-number-lines-words-characters-paragraphs-text-file-using-java/)
+* [Corey Schafer - Youtube channel on Regex](https://www.youtube.com/watch?v=K8L6KVGG-7o)
--- challenge2.py
+++ challenge2.py
@@ -1,48 +1,108 @@
 # Import the python sys library to read a file
 
+from enum import unique
 import sys
+import re
 
-# assign an empty dictionary as word_string
-# using the with open() in the sys library to read an external file name " demo.txt" as a f variable
-# use the read () and split () to read the demo.txt and separate each word as string and assigned 
-# into the empty dictionary word_string
-word_string =  {}
-with open('demo.txt') as f:
-    word_string = f.read().split()
-print(word_string)  # use print to debug and see if it print out each word as a string
+print("Welcome !, this demo is using text content from the novel 'Oliver Twisted' Chapter III.\n")
 
 # Create a function call to count how many characters in the file
-def count_char ():
+def count_char (letter):
+    x = [] # create an new array list to separate each alpahbet letter in the for loop
+    y =""
+    new_word = re.sub(r'[^\w\s]', y, letter) # Using regex and sub () method to deliminated the punctuation marks and assign to a new string (y)
+    new_word = new_word.split() # Using the split () to deliminate all the white spaces
+    # print(new_word)
+    for n in new_word: # using the for loop to add each alphabet letter to the x array list
+        x += n
+    return print("Total number of characters in this file: " + str(len(x))) # using the len() to determine the length of the contents
+    # return print(x)
+
+# Create a function call to count how many words in the file
+def count_words (letter):
+    y = ""
+    cont = 0
+    new_word = re.sub(r'[^\w\s]', y, letter) # Using regex and sub () method to deliminated the punctuation marks - \s new line, tab, ^ = not, \w = word character
+    new_word = new_word.split() # Using the split () to deliminate all the white spaces
+    # print(new_word)
+    # new_array = []
+    for n in new_word:
+        # print(letter)
+        # new_array += n
+        cont += 1
+        # print(cont)
+    return print("Total number of words count in this file: " + str(cont))
+
+
+# Create a function call to count how many sentence in the file
+def count_sent (letter):
+    cnt = 0
+    for n in letter:
+        if n.endswith("."): # the endswith() method will search for a period at the end of the sentence
+            cnt += 1
+            # print(n)
+    return print("Total number of sentences in this file: " + str(cnt))
+
+# Create a function call to count how many paragraph in the file
+def count_para (letter):
+    # print(letter)
+    paragraphCount = 0
+    x = re.findall('\n\n',letter)  # The findall() method will find all the white spaces -- \n\n = empty lines
+    for n in x:
+        paragraphCount += 1
+    # print(paragraphCount)
+  
+    return print("Total number of paragraphs in this file: " + str(paragraphCount))
+
+# Create a function call to count how many unique word in the file
+def count_bigr (letter):
+  # Dictionary for counting the words
+    y = ""
+    new_word = re.sub(r'[^\w\s]', y, letter) # Using regex and sub () method to deliminated the punctuation marks - \s new line, tab, ^ = not, \w = word character
+    new_word = new_word.split()
+    word_count = {}
+    # print(new_word)
+    for line in new_word:
+        if line not in word_count:
+            word_count[line] = 1
+        else:
+            word_count[line] += 1
+ 
+    return print("Total number of unique pairs of words in this file: " + str(word_count))
+
+# This function will replace all the "a" or "A" to "@" and all the "e" and "E" to "3"
+def funker_words(phrase):
+    new_phrase = ''
+    for letter in phrase:
+        if letter == 'A' or letter == 'a':
+            new_phrase += '@'
+        elif letter == 'E' or letter == 'e':
+            new_phrase += '3'
+        elif letter == 'I' or letter == 'i':
+            new_phrase += '!'
+        elif letter == 'G' or letter == 'g':
+            new_phrase += '9'
+        else:
+            new_phrase += letter
+    return print(new_phrase)
+
+with open('demo.txt', 'r') as f:
+    word_string = f.read()   # the read () method read the contents from the file and assigned a variable "word_string" as a place holder for those contents
+    # y = re.sub(r'[^\w\s]', y, word_string)  # using the regex (regular expression) library imported and sub () method in python to - search for pattern (punctuation and special characters)
+    # word_string = y.split() # use the split() to remove all white spaces
+    # print(word_string)
+
+    count_char(word_string) 
+    count_words(word_string) 
+    count_sent(word_string) 
+    count_para(word_string) 
+    count_bigr(word_string) 
+    funker_words(word_string)
+    
+  
     
 
-    
-    return
-
-# Create a function call to cound how many words in the file
-
-def count_words ():
 
 
-    return 
-
-# Create a function call to cound how many sentence in the file
-def count_sent ():
-
-
-
-    return
-
-# Create a function call to cound how many paragraph in the file
-
-def count_para ():
-
-
-    return
-
-# Create a function call to cound how many unique word in the file
-
-def count_bigr ():
-
-
-
-    return
+# assign y as an empty word_string - for place holder
+# using the with open() in the sys library to read an external file name " demo.txt" as a f variable
